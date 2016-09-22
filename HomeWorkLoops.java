ebfb2f0fb1c145a0df0e39e87f4844b12679ed6f
Matthew Keller <mkeller2019@pingry.org>
2016-09-21 20:43:50 -0400

    contains working program and ReadMe

---
+++ HomeWorkLoops.java
@@ -0,0 +1,62 @@
+/**
+* Program that prints two designs of stars
+* using nested for loops.
+* @author Matt Keller
+* @version 1.0
+*/
+
+public class HomeworkLoops
+{
+	public static void main(String[] args)
+	{
+		
+	
+	forLoop(4);
+	System.out.println("-------------------");
+	forLoop2(4);
+	
+	}
+
+
+	public static void forLoop(int n)
+	{
+		for(int i = n; i > 0; i --)
+		{
+			for(int p = 1; p <= i; p ++)
+			{
+				System.out.print("*");
+			
+			}
+		System.out.println();
+		
+		
+		}
+	
+	
+	
+	}
+
+
+	public static void forLoop2(int n)
+	{
+	
+		for(int i = 1; i <= n; i ++)
+		{
+			for(int p = 1; p <= i; p ++)
+			{
+				System.out.print("*");
+			
+			}
+		System.out.println();
+		}
+	
+	
+	
+	
+	}
+
+
+
+
+}
+
